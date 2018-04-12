f9a16e1f35ed854b1658a8e983ee871fc0ea1ff9
Thomas Barratt <thbarratt1@sheffield.ac.uk>
2018-04-12 17:05:47 +0100

    Added Processwire for PHP DB

---
+++ site/config.php
@@ -0,0 +1,122 @@
+<?php namespace ProcessWire;
+
+/**
+ * ProcessWire Configuration File
+ *
+ * Site-specific configuration for ProcessWire
+ * 
+ * Please see the file /wire/config.php which contains all configuration options you may
+ * specify here. Simply copy any of the configuration options from that file and paste
+ * them into this file in order to modify them. 
+ * 
+ * SECURITY NOTICE
+ * In non-dedicated environments, you should lock down the permissions of this file so
+ * that it cannot be seen by other users on the system. For more information, please
+ * see the config.php section at: https://processwire.com/docs/security/file-permissions/
+ * 
+ * This file is licensed under the MIT license
+ * https://processwire.com/about/license/mit/
+ *
+ * ProcessWire 3.x, Copyright 2016 by Ryan Cramer
+ * https://processwire.com
+ *
+ */
+
+if(!defined("PROCESSWIRE")) die();
+
+/*** SITE CONFIG *************************************************************************/
+
+/** @var Config $config */
+
+/**
+ * Enable debug mode?
+ *
+ * Debug mode causes additional info to appear for use during dev and debugging.
+ * This is almost always recommended for sites in development. However, you should
+ * always have this disabled for live/production sites.
+ *
+ * @var bool
+ *
+ */
+$config->debug = false;
+
+/**
+ * Prepend template file
+ *
+ * PHP file in /site/templates/ that will be loaded before each page's template file.
+ * Example: _init.php
+ *
+ * @var string
+ *
+ */
+$config->prependTemplateFile = '_init.php';
+
+/**
+ * Append template file
+ *
+ * PHP file in /site/templates/ that will be loaded after each page's template file.
+ * Example: _main.php
+ *
+ * @var string
+ *
+ */
+$config->appendTemplateFile = '_main.php';
+
+
+
+
+/*** INSTALLER CONFIG ********************************************************************/
+
+/**
+ * Installer: Database Configuration
+ * 
+ */
+$config->dbHost = 'localhost';
+$config->dbName = 'mats';
+$config->dbUser = 'dev';
+$config->dbPass = 'MTMrwZwRD26EJ58G';
+$config->dbPort = '3306';
+
+/**
+ * Installer: User Authentication Salt 
+ * 
+ * Must be retained if you migrate your site from one server to another
+ * 
+ */
+$config->userAuthSalt = 'ee5aa9492e9be893113bae0f06d019ad'; 
+
+/**
+ * Installer: File Permission Configuration
+ * 
+ */
+$config->chmodDir = '0777'; // permission for directories created by ProcessWire
+$config->chmodFile = '0666'; // permission for files created by ProcessWire 
+
+/**
+ * Installer: Time zone setting
+ * 
+ */
+$config->timezone = 'Europe/Berlin';
+
+/**
+ * Installer: Admin theme
+ * 
+ */
+$config->defaultAdminTheme = 'AdminThemeUikit';
+
+/**
+ * Installer: Unix timestamp of date/time installed
+ * 
+ * This is used to detect which when certain behaviors must be backwards compatible.
+ * Please leave this value as-is.
+ * 
+ */
+$config->installed = 1523548943;
+
+
+/**
+ * Installer: HTTP Hosts Whitelist
+ * 
+ */
+$config->httpHosts = array('localhost', 'mats.atticcs.co.uk');
+
