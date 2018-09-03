5faf936ca540baf1647b117e56c9749172892234
Thomas Barratt <tb@thomasbarratt.co.uk>
2018-09-03 22:41:20 +0100

    workspace link to UI setup

--- public_html/site/templates/assets/pages/mat.php
+++ public_html/site/templates/assets/pages/mat.php
@@ -1,3 +1,11 @@
 <script src=""></script>
-<div class="ui-wrapper" id="ui-wrapper">
-</div>
+    <link rel="manifest" href="<?= $config->urls->assets?>ui/public/manifest.json">
+    <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css"> -->
+    <link rel="stylesheet" type="text/css" href="<?= $config->urls->assets?>ui/public/styles/main.css">
+    <link rel="shortcut icon" href="<?= $config->urls->assets?>ui/public/favicon.ico">
+
+ <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+  </body>
