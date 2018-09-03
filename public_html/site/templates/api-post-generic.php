ec4e2be5ccbbfe40beb638ce4ac3ca978119ac40
Thomas Barratt <tb@thomasbarratt.co.uk>
2018-09-03 10:21:53 +0100

    Added basic post page

---
+++ public_html/site/templates/api-post-generic.php
@@ -0,0 +1,11 @@
+<?php namespace ProcessWire;
+// home.php (homepage) template file. 
+// See README.txt for more information
+// Primary content is the page body copy and navigation to children. 
+// See the _func.php file for the renderNav() function example
+// $content = $page->body . renderNav($page->children); 
+// $search_string = "template=element,id="+$_GET['id'];
+// $fields = ['title', 'created','image','id'];
+// $template_name = "api-call";
+
+$template = "element";
--- public_html/site/templates/assets/pages/api-post.php
+++ public_html/site/templates/assets/pages/api-post.php
@@ -3,3 +3,17 @@
 if($config->ajax) {
     // page was requested from ajax
 }
+
+$p = new Page(); // create new page object
+$p->template = 'element'; // set template
+$p->parent = wire('pages')->get('/elements/'); // set the parent
+$p->title = $_POST['name']; // give it a name used in the url for the page
+
+// $p->title = 'My New Page'; // set page title (not neccessary but recommended)
+
+$p->save();
+// $p->tags  = $_POST['tags'];
+
+// populate fields
+
+// $p->save();
