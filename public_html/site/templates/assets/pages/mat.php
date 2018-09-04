f36731b760b438f67f9fea7b0ad198f70bba26dd
Thomas Barratt <tb@thomasbarratt.co.uk>
2018-09-04 09:48:46 +0100

    Altered path to ui

--- public_html/site/templates/assets/pages/mat.php
+++ public_html/site/templates/assets/pages/mat.php
@@ -1,13 +1,13 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-<meta name="theme-color" content="#000000"><link rel="manifest" href="<?= $config->urls->assets?>ui/manifest.json">
+<meta name="theme-color" content="#000000"><link rel="manifest" href="<?= $config->urls->assets?>cw-ui/manifest.json">
 <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css"> -->
-<link rel="stylesheet" href="<?= $config->urls->assets?>ui/styles/main.css"><link rel="shortcut icon" href="<?= $config->urls->assets?>ui/favicon.ico">
-<link href="/static/css/main.0c5dad92.css" rel="stylesheet">
+<link rel="stylesheet" href="<?= $config->urls->assets?>cw-ui/styles/main.css"><link rel="shortcut icon" href="<?= $config->urls->assets?>cw-ui/favicon.ico">
+<link href="<?= $config->urls->assets?>cw-ui/static/css/main.0c5dad92.css" rel="stylesheet">
 
 <noscript>
     You need to enable JavaScript to run this app.
 </noscript>
 <div id="root"></div>
-<script type="text/javascript" src="<?= $config->urls->assets?>ui/static/js/main.220ea57e.js"></script>
+<script type="text/javascript" src="<?= $config->urls->assets?>cw-ui/static/js/main.220ea57e.js"></script>
 </body>
