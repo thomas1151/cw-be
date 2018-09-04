04603c9f28f06e426b5afa771fbc8cb80552b056
Thomas Barratt <tb@thomasbarratt.co.uk>
2018-09-04 09:31:43 +0100

    Edited cw page to allow for new prod build of ui

--- public_html/site/templates/assets/pages/mat.php
+++ public_html/site/templates/assets/pages/mat.php
@@ -1,11 +1,13 @@
-<script src=""></script>
-    <link rel="manifest" href="<?= $config->urls->assets?>ui/public/manifest.json">
-    <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css"> -->
-    <link rel="stylesheet" type="text/css" href="<?= $config->urls->assets?>ui/public/styles/main.css">
-    <link rel="shortcut icon" href="<?= $config->urls->assets?>ui/public/favicon.ico">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+<meta name="theme-color" content="#000000"><link rel="manifest" href="<?= $config->urls->assets?>ui/manifest.json">
+<!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css"> -->
+<link rel="stylesheet" href="<?= $config->urls->assets?>ui/styles/main.css"><link rel="shortcut icon" href="<?= $config->urls->assets?>ui/favicon.ico">
+<link href="/static/css/main.0c5dad92.css" rel="stylesheet">
 
- <noscript>
-      You need to enable JavaScript to run this app.
-    </noscript>
-    <div id="root"></div>
-  </body>
+<noscript>
+    You need to enable JavaScript to run this app.
+</noscript>
+<div id="root"></div>
+<script type="text/javascript" src="<?= $config->urls->assets?>ui/static/js/main.220ea57e.js"></script>
+</body>
